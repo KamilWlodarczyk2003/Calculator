925a0bc6008855b66b43c5f6f316393ecaed5972
Kamil Wlodarczyk <KamilWlodarczyk2003@gmail.com>
2023-04-21 22:40:14 +0200

    finished calculator

--- calculator.py
+++ calculator.py
@@ -1,9 +1,7 @@
-﻿
+﻿import os
+from art import logo
+
 #Add
-
-from pyclbr import Function
-from unittest import result
-
 
 def Add(n1,n2):
     return n1+n2
@@ -23,16 +21,27 @@ def Multiply(n1,n2):
 def Divide(n1,n2):
     return n1/n2
 
+print(logo)
+
 Functions={"+": Add, "-": Subtract, "*":Multiply, "/":Divide}
 
 num1=int(input("What's your first number?: "))
 
-for x in Functions:
-    print(x)
-operation_symbol=input("Pick an operator from the list above: ")
+answer="yes"
 
-num2=int(input("What's your first number?: "))
+while answer=="yes":
+    for x in Functions:
+        print(x)
+    operation_symbol=input("Pick an operator from the list above: ")
 
-result=Functions[operation_symbol](num1,num2)
+    num2=int(input("What's your second number?: "))
 
-print(f"{num1} {operation_symbol} {num2} = {result}")
+    result=Functions[operation_symbol](num1,num2)
+
+    print(f"{num1} {operation_symbol} {num2} = {result}")
+
+    answer=input('Type "yes" if you want to continue with previous result: ')
+    if answer=="yes":
+        os.system('cls' if os.name == 'nt' else 'clear')
+        num1=result
+
