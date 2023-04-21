f0a32e1df645ab732eacabe3d0cf527b816e1ee9
Kamil Wlodarczyk <KamilWlodarczyk2003@gmail.com>
2023-04-21 22:12:10 +0200

    Added functionality

--- calculator.py
+++ calculator.py
@@ -1,6 +1,10 @@
 ﻿
 #Add
 
+from pyclbr import Function
+from unittest import result
+
+
 def Add(n1,n2):
     return n1+n2
 
@@ -18,3 +22,17 @@ def Multiply(n1,n2):
 
 def Divide(n1,n2):
     return n1/n2
+
+Functions={"+": Add, "-": Subtract, "*":Multiply, "/":Divide}
+
+num1=int(input("What's your first number?: "))
+
+for x in Functions:
+    print(x)
+operation_symbol=input("Pick an operator from the list above: ")
+
+num2=int(input("What's your first number?: "))
+
+result=Functions[operation_symbol](num1,num2)
+
+print(f"{num1} {operation_symbol} {num2} = {result}")
